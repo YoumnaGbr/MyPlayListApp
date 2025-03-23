98e56e461f0a7a67211b602159973f03e1e0df59
youmna.Gabr <youmna.Gabr@DESKTOP-TC11CQ5>
2025-03-23 11:39:08 +0200

    #Move Script in index to site.js

--- MyPlayListApp/wwwroot/js/site.js
+++ MyPlayListApp/wwwroot/js/site.js
@@ -1,4 +1,47 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    $('#editSongModal').on('show.bs.modal', function (event) {
+        const button = $(event.relatedTarget);
+        const modal = $(this);
 
-// Write your JavaScript code.
+        modal.find('#Id').val(button.data('id'));
+        modal.find('#Name').val(button.data('name'));
+        modal.find('#SingerId').val(button.data('singerid'));
+        modal.find('#CategoryId').val(button.data('categoryid'));
+    });
+
+    $('.singer-details-link').on('click', function (event) {
+        event.preventDefault();
+
+        var singerId = $(this).data('id');
+
+        $.ajax({
+            url: getSingerUrl,
+            type: 'GET',
+            data: { id: singerId },
+            success: function (data) {
+                $('#modalContent').html(data);
+            },
+            error: function () {
+                $('#modalContent').html('<p class="text-danger">Failed to load singer details.</p>');
+            }
+        });
+    });
+});
+
+function deleteSong(songId) {
+    if (confirm("Are you sure you want to delete this song?")) {
+        $.ajax({
+            type: "POST",
+            dataType: 'json',
+            url: deleteSongUrl,
+            data: { songId: songId },
+            success: function (e) {
+                alert(e.message);
+                window.location.reload();
+            },
+            error: function (e) {
+                alert(e.message);
+            }
+        });
+    }
+}
